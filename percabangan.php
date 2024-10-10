b642e6cf741aca6e1e254273c6c2efa90b3d4d32
hafidznasution <rhafis709@gmail.com>
2024-10-10 11:00:29 +0700

    algoritma 1

---
+++ percabangan.php
@@ -0,0 +1,9 @@
+<?php 
+$sisi1 = 16;
+$sisi2 = 16;
+// Meminta input dari pengguna 
+// Menghitung luas persegi 4
+$luas = $sisi1 * $sisi2; 
+// Menampilkan hasil echo "Luas persegi adalah: " . 
+echo "luas persegi panjang adalah=  $luas";
+?>
